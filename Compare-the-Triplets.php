92ba7584f8098e9ec4894d0f8ace5ac8df83adee
Raid Boulahdid <raidski20@gmail.com>
2023-12-25 19:44:58 +0100

    Updated Compare-the-Triplets solution

--- Compare-the-Triplets.php
+++ Compare-the-Triplets.php
@@ -1,38 +1,45 @@
 <?php
 
 $handle = fopen ("php://stdin", "r");
-function solve($a0, $a1, $a2, $b0, $b1, $b2){
-    // Complete this function
-    $alice = 0;
-    $bob = 0;
 
-    if ($a0 > $b0) {
-        $alice++;
+function compareTriplets($aliceArr, $bobArr) {
+    
+    if(count($aliceArr) !== count($bobArr)) {
+        throw new exception('Both arrays must have the same length');
     }
-
-    if ($a1 > $b1) {
-        $alice++;
+    
+    $aliceScore = 0;
+    $bobScore = 0;
+    
+    foreach ($aliceArr as $key => $value)
+    {
+        if($aliceArr[$key] < $bobArr[$key]) {
+            $bobScore++;
+        } elseif($aliceArr[$key] > $bobArr[$key])  {
+            $aliceScore++;
+        }
     }
-    if ($a2 > $b2) {
-        $alice++;
-    }
-
-    if ($b0 > $a0) {
-        $bob++;
-    }
-    if ($b1 > $a1) {
-        $bob++;
-    }
-    if ($b2 > $a2) {
-        $bob++;
-    }
-
-    return print("$alice $bob");
+    
+    return [$aliceScore, $bobScore];
 }
 
-fscanf($handle, "%d %d %d", $a0, $a1, $a2);
-fscanf($handle, "%d %d %d", $b0, $b1, $b2);
-$result = solve($a0, $a1, $a2, $b0, $b1, $b2);
-echo implode(" ", $result)."\n";
+echo 'Enter Alice\'s array size: '; 
+fscanf($handle, "%i",$n);
+$aliceArr = array();
+for($a_i = 0; $a_i < $n; $a_i++) {
+   $a_temp = fgets($handle);
+   $aliceArr[] = explode(" ",$a_temp);
+   $aliceArr[$a_i] = array_map('intval', $aliceArr[$a_i]);
+}
 
-?>
+echo 'Enter Bob\'s array size: '; 
+fscanf($handle, "%i",$n);
+$bobArr = array();
+for($a_i = 0; $a_i < $n; $a_i++) {
+   $a_temp = fgets($handle);
+   $bobArr[] = explode(" ",$a_temp); 
+   $bobArr[$a_i] = array_map('intval', $bobArr[$a_i]); 
+}
+
+$result = compareTriplets($aliceArr, $bobArr);
+echo $result[0] . ' ' . $result[1] . "\n";
