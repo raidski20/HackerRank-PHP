b7cfd169610333baee68517f0d6f69616622a6ea
Raid Boulahdid <raidski20@gmail.com>
2023-12-25 20:10:18 +0100

    Updated Mini-Max-Sum solution

--- mini-max-sum.php
+++ mini-max-sum.php
@@ -1,23 +1,22 @@
 <?php
 
-// Complete the miniMaxSum function below.
 function miniMaxSum($arr) {
-    sort($arr);
-    $total = array_sum($arr);
-    $newsum = [];    
-    for ($i=0; $i < count($arr); $i++) {
-        $newsum[] = $total-$arr[$i];
+    if(count($arr) > 5) {
+        throw new Exception('Array size not supported');
     }
-    $min = min($newsum);
-    $max = max($newsum);
     
-//    $lowarr = array($arr[0],$arr[1],$arr[2],$arr[3]);
-//    $higharr = array($arr[1],$arr[2],$arr[3],$arr[4]);
+    $iterationsSums = [];
+    $arrDup = [];
     
-//    $min = array_sum($lowarr);
-//    $max = array_sum($higharr);
-    
-    echo $min.' '.$max;
+    foreach($arr as $key => $number)
+    {
+        $arrDup = $arr;
+        unset($arrDup[$key]);
+        
+        $iterationsSums[] = array_sum($arrDup);
+    }
+        
+    echo min($iterationsSums) . ' ' . max($iterationsSums);
 }
 
 $stdin = fopen("php://stdin", "r");
