31ee3be69756da1d98422cb7ece5e674a84ef783
Raid Boulahdid <raidski20@gmail.com>
2023-12-26 12:10:04 +0100

    Refactored Time-Conversion solution

--- Time-Conversion.php
+++ Time-Conversion.php
@@ -1,28 +1,22 @@
 <?php
 
 function timeConversion($s) {
-
     $time= explode(":", $s);
     $pos = strpos(end($time), 'PM');
 
-    $hr;
-    $min;
-    $sec;
+    $hr = $time[0];
+    $min = $time[1];
+    $sec = intval(end($time));
 
     if ($pos === false) {
-        $hr= ($time[0] == 12) ? '00' : $time[0];
-        $min= $time[1];
-        $t= intval(end($time));
-        $sec= ($t<10) ? "0$t": $t;
+        $hr = ($hr == 12) ? '00' : $hr;
+    } else {
+        $hr = ($hr == 12) ? 12 : (12 + $hr);
     }
-    else
-    {
-        $hr= ($time[0] == 12) ? 12 : 12+$time[0];
-        $min= $time[1];
-        $t= intval(end($time));
-        $sec= ($t<10) ? "0$t": $t;
-}
-return $hr.':'.$min.':'.$sec;    
+
+    $sec = ($sec < 10) ? "0$sec" : $sec;
+
+    return $hr . ':' . $min. ':' . $sec;
 }
 
 $fptr = fopen(getenv("OUTPUT_PATH"), "w");
